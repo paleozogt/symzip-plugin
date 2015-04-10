759b0d042025c842804c2ac398d618201914a771
Aaron Simmons <asimmons@sensoryinc.com>
2015-04-10 17:37:56 -0600

    stubbingout InfoZipTask like gradle's Zip task

--- plugin/src/main/groovy/org/paleozogt/gradle/infozip/InfoZipTask.groovy
+++ plugin/src/main/groovy/org/paleozogt/gradle/infozip/InfoZipTask.groovy
@@ -1,9 +1,62 @@
 package org.paleozogt.gradle.infozip
 
-import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.bundling.AbstractArchiveTask
+import org.gradle.api.internal.file.copy.CopyAction
 import org.gradle.api.tasks.TaskAction
+import org.gradle.api.GradleException;
+import org.gradle.api.tasks.WorkResult;
 
-class InfoZipTask extends DefaultTask {
+import org.gradle.api.internal.file.copy.CopyActionProcessingStream;
+import org.gradle.api.internal.file.CopyActionProcessingStreamAction;
+import org.gradle.api.internal.tasks.SimpleWorkResult;
+import org.gradle.api.internal.file.copy.FileCopyDetailsInternal;
+import org.gradle.api.file.FileCopyDetails;
 
+class InfoZipTask extends AbstractArchiveTask {
 
+    @Override
+    protected CopyAction createCopyAction() {
+        return new ZipCopyAction(getArchivePath());
+    }
+
+    public class ZipCopyAction implements CopyAction {
+        private final File zipFile;
+
+        public ZipCopyAction(File zipFile) {
+            this.zipFile = zipFile;
+        }
+
+        public WorkResult execute(final CopyActionProcessingStream stream) {
+            stream.process(new StreamAction());
+            return new SimpleWorkResult(true);
+        }
+
+        private class StreamAction implements CopyActionProcessingStreamAction {
+            public StreamAction() {
+            }
+
+            public void processFile(FileCopyDetailsInternal details) {
+                if (details.isDirectory()) {
+                    visitDir(details);
+                } else {
+                    visitFile(details);
+                }
+            }
+
+            private void visitFile(FileCopyDetails fileDetails) {
+                try {
+
+                } catch (Exception e) {
+                    throw new GradleException(String.format("Could not add %s to ZIP '%s'.", fileDetails, zipFile), e);
+                }
+            }
+
+            private void visitDir(FileCopyDetails dirDetails) {
+                try {
+                } catch (Exception e) {
+                    throw new GradleException(String.format("Could not add %s to ZIP '%s'.", dirDetails, zipFile), e);
+                }
+            }
+        }
+    }
 }
