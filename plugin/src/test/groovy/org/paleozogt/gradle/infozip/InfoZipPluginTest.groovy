4c3f533cacc4c21984f6a75bae3e86453a233d2d
Aaron Simmons <asimmons@sensoryinc.com>
2015-04-10 16:09:01 -0600

    fixing copy/paste err

--- plugin/src/test/groovy/org/paleozogt/gradle/infozip/InfoZipPluginTest.groovy
+++ plugin/src/test/groovy/org/paleozogt/gradle/infozip/InfoZipPluginTest.groovy
@@ -1,4 +1,4 @@
-package com.crashlytics.examples.gradle
+package org.paleozogt.gradle.infozip
 
 import org.junit.Test
 import org.gradle.testfixtures.ProjectBuilder
@@ -8,11 +8,8 @@ import static org.junit.Assert.*
 class PreprocessorPluginTest {
 
     @Test
-    public void crashlyticsPluginHasCustomizableManifest() {
+    public void applyTest() {
         Project project = ProjectBuilder.builder().build()
         project.apply plugin: 'org.paleozogt.infozip'
-        
-        project.task(type:Zip) {
-        }
     }
 }
