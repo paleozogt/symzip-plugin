b03f7388f3b0f36150b7f3a443dc5f8430b2bdc8
Aaron Simmons <asimmons@sensoryinc.com>
2015-04-10 16:37:43 -0600

    running a test build.gradle as part of unit-tests

--- plugin/src/test/groovy/org/paleozogt/gradle/infozip/InfoZipPluginTest.groovy
+++ plugin/src/test/groovy/org/paleozogt/gradle/infozip/InfoZipPluginTest.groovy
@@ -1,8 +1,12 @@
 package org.paleozogt.gradle.infozip
 
-import org.junit.Test
-import org.gradle.testfixtures.ProjectBuilder
 import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import org.gradle.tooling.GradleConnector;
+import org.gradle.tooling.ProjectConnection;
+import org.gradle.tooling.BuildLauncher;
+
+import org.junit.Test
 import static org.junit.Assert.*
 
 class PreprocessorPluginTest {
@@ -11,5 +15,19 @@ class PreprocessorPluginTest {
     public void applyTest() {
         Project project = ProjectBuilder.builder().build()
         project.apply plugin: 'org.paleozogt.infozip'
+    }
+
+    @Test
+    public void sampleBuildTest() {
+        GradleConnector connector = GradleConnector.newConnector()
+        connector.forProjectDirectory(new File("src/test/resources/test-build"))
+        ProjectConnection connection = connector.connect()
+        try {
+            BuildLauncher launcher = connection.newBuild()
+            launcher.forTasks("build")
+            launcher.run()
+        } finally {
+            connection.close()
+        }
     }
 }
